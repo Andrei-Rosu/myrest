4791393a7430265a72529d95c702a4c2072b570a
cevantime <cevantime@cevantime-N56VZ>
2016-02-28 21:59:16 +0100

    Merge origin/master
    Conflicts:
    	README.md

--- application/config/database.php
+++ application/config/database.php
@@ -52,9 +52,9 @@ $active_group = getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'default
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'myrest';
-$db['default']['password'] = '5b2z6tFt69ZAjef8';
-$db['default']['database'] = 'myrest';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
@@ -66,6 +66,22 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '{PRE}';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
+
+$db['recette']['hostname'] = 'localhost';
+$db['recette']['username'] = 'myrest';
+$db['recette']['password'] = '5b2z6tFt69ZAjef8';
+$db['recette']['database'] = 'myrest';
+$db['recette']['dbdriver'] = 'mysqli';
+$db['recette']['dbprefix'] = '';
+$db['recette']['pconnect'] = FALSE;
+$db['recette']['db_debug'] = TRUE;
+$db['recette']['cache_on'] = FALSE;
+$db['recette']['cachedir'] = '';
+$db['recette']['char_set'] = 'utf8';
+$db['recette']['dbcollat'] = 'utf8_general_ci';
+$db['recette']['swap_pre'] = '{PRE}';
+$db['recette']['autoinit'] = TRUE;
+$db['recette']['stricton'] = FALSE;
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
